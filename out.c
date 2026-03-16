b1682399c2f339b50eff35b8efb4dc20a353b4b9
AnshuKumar25 <anshukumarnew25@gmail.com>
2026-03-16 12:29:30 +0530

    Initial commit

---
+++ out.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void){
+float x;
+float y;
+x = 10;
+y = 5;
+printf("%.2f\n", (float)(x));printf("%.2f\n", (float)(y));if(x>y){
+printf("x is greater\n");
+}
+return 0;
+}
